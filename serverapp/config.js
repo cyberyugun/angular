aa810f7e1f388a0cce7dd1a27bbe33b1d434c5f3
yudi gunawan <yudigunawan52@gmail.com>
2019-06-11 09:04:25 +0700

    one day

---
+++ serverapp/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    DB: 'mongodb://localhost:27017/Account',
+    secret:process.env.SECRET || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9sYXR1bmRlZ2FydWJhQGdtYWlsLmNvbSIsImZ1bGxOYW1lIjoiT2xhdHVuZGUgR2FydWJhIiwiX2lkIjoiNThmMjYzNDdiMTY1YzUxODM1NDMxYTNkIiwiaWF0IjoxNDkyMjgwMTk4fQ.VcMpybz08cB5PsrMSr25En4_EwCGWZVFgciO4M-3ENE'
+  };
+  
